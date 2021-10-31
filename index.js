b4221a08748413c9cecbdd2d4a6ac6d30295a14b
MDRomzan <mdromjan483@gmail.com>
2021-10-31 11:02:53 +0600

    aded

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { MongoClient } = require('mongodb');
+const ObjectId=require("mongodb").ObjectId;
 const express = require('express');
 require("dotenv").config();
 const cors= require("cors");
@@ -31,6 +32,53 @@ async function run() {
         const services=await cursor.toArray();
         res.send(services);
     });
+// GET SINGLE SERVICE
+app.get("/services/:id",async(req,res)=>{
+    const id=req.params.id;
+    // console.log("grttiig one id",id);
+    const query={_id:ObjectId(id)}
+    const service= await servicesCollection.findOne(query);
+    res.json(service);
+
+})
+// get delete id
+app.delete("/services/:id",async(req,res)=>{
+    const id=req.params.id;
+    const query={_id:ObjectId(id)};
+    const result=await servicesCollection.deleteOne(query);
+    res.json(result);
+})
+
+    // post add services
+    app.post("/services",async(req,res)=>{
+        const service=req.body;
+        console.log("hiiting the post api",service);
+        const result=await servicesCollection.insertOne(service);
+        console.log(result);
+        res.json(result);
+        //res.send("posted hitt")
+    });
+    // update user      
+    app.put("/services/:id",async(req,res)=>{
+        const id=req.params.id;
+        // console.log(id);
+        const updateService=req.body;
+        // console.log(updateService);
+        const filter={_id:ObjectId(id)};
+        const options ={upsert:true};
+        const updateDoc ={
+            $set:{
+                name:updateService.name,
+                img:updateService.img,
+                price:updateService.price,
+                Description: updateService.Description
+            },
+        };
+        const result=await servicesCollection.updateOne(filter,options,updateDoc);
+        console.log("updateing Service ", req);
+        res.json(result)
+    });
+
     // Add post order
     app.post("/orders",async(req,res)=>{
         const order=req.body;
